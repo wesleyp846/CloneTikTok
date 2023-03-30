c9765e0bcbc6ae1125c63c206c31c82d9587e64d
Wesley Pereira <aventureiro-msn@hotmail.com>
2023-03-29 23:06:45 -0300

    Up do video

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -22,7 +22,7 @@ function Video() {
         ref={videoRef}
         onClick={handlleStart}
         loop
-        src="https://v16-webapp-prime.tiktok.com/video/tos/maliva/tos-maliva-ve-0068c800-us/d0bc13229eef4e10b630773b26278f72/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0|0|0|0&br=2458&bt=1229&cs=0&ds=3&ft=_RwJrBiTq8Zmotg7Cc_vjqy_8AhLrus&mime_type=video_mp4&qs=0&rc=OGc3ZDg2NDM2OjNoNWloO0BpM3A0bnNucnl4dTMzZjczM0BeYTFeMGM1XjAxYzMyMmI1YSNoMjBuMi4uczZfLS0xMTZzcw==&btag=80000&expire=1680159096&l=202303300051303E65F8E9AD833A84A784&ply_type=2&policy=2&signature=1659d74ae740e6c35322b0a3a04f748a&tk=tt_chain_token"
+        src="  https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
       ></video>
     </div>
   );

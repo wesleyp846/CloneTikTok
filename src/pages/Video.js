6ca7fd98c6b8d8b3d6cf68e58bbba3edb9986c9c
Wesley Pereira <aventureiro-msn@hotmail.com>
2023-03-29 21:08:00 -0300

    Esquetetizando

---
+++ src/pages/Video.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./video.css"
+
+function Video() {
+  return (
+    <div className='video'>
+        Eu sou um componente de video
+        <video className='video__player'>
+
+        </video>
+    </div>
+  )
+}
+
+export default Video
